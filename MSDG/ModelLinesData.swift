69d28ec14b1d18182e077f08e17e8949f8184365
Juan Morillo <juanmorillios@gmail.com>
2017-05-05 18:43:18 +0200

    Create Model of Data

---
+++ MSDG/ModelLinesData.swift
@@ -0,0 +1,13 @@
+//
+//  ModelLinesData.swift
+//  MSDG
+//
+//  Created by Juan Morillo on 5/5/17.
+//  Copyright © 2017 Juan Morillo. All rights reserved.
+//
+
+import UIKit
+
+class ModelLinesData: NSObject {
+
+}
