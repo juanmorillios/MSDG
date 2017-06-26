cb5279d58fa3f75e89c22f8c4c186d3531297d3b
Juan Morillo <juanmorillios@gmail.com>
2017-06-26 23:47:58 +0200

    Trabajando con labels, alerts y functions

--- MSDG/RutasTableViewController.swift
+++ MSDG/RutasTableViewController.swift
@@ -14,17 +14,4 @@ class RutasTableViewController: UITableViewController {
         super.viewDidLoad()
 
          }
-
-       // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-              return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-      
-        return 0
-    }
-
-   
 }
