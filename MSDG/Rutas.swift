b6b9c0b5a4cbc1faed2cb1ca7e00462efc1acb9e
Juan Morillo <juanmorillios@gmail.com>
2017-07-08 23:35:02 +0200

    Trabajando con clases e inicializadores

---
+++ MSDG/Rutas.swift
@@ -0,0 +1,32 @@
+//
+//  Rutas.swift
+//  MSDG
+//
+//  Created by Juan Morillo on 8/7/17.
+//  Copyright © 2017 Juan Morillo. All rights reserved.
+//
+
+import UIKit
+
+class Ruta2: NSObject {
+  
+  var name : String?
+  var coordenada : Int?
+  
+  init(name: String, coordenada: Int) {
+  
+    self.name = name
+    self.coordenada = coordenada
+    
+    func obtenerRutas(name: String) -> String {
+    
+      return name
+    
+    }
+ 
+  }
+
+}
+
+
+
