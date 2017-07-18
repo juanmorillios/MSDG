8e0f635a2a7e79931412a6d2120ba982806066d4
Juan Morillo <juanmorillios@gmail.com>
2017-07-18 23:47:23 +0200

    Trabajando con la capa de Conexión

---
+++ MSDG/Metro.swift
@@ -0,0 +1,13 @@
+//
+//  Metro.swift
+//  MSDG
+//
+//  Created by Juan Morillo on 18/7/17.
+//  Copyright © 2017 Juan Morillo. All rights reserved.
+//
+
+import UIKit
+
+class Metro: NSObject {
+
+}
